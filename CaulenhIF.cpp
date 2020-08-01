773a5ef1fb6f598e3aeb267366b2008b8fcb0681
namlem4u <68719961+namlem4u@users.noreply.github.com>
2020-08-01 21:35:20 +0700

    Add files via upload

---
+++ CaulenhIF.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+printf("nhap a va b \t");
+scanf("%d%d",&a,&b);
+if(a!=0)
+{
+if (a!0)
+{
+  /* code */
+}
+
+ if(b>0)
+  printf("a khac 0 va b>0");
+else printf("a bang 0 va b<0");
+}
+return 0; 
+}
